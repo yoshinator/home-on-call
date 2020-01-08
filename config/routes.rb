85cf580657163269dff23ac8621c4c5e56f3d086
yoshinator <yoanante@gmail.com>
2020-01-07 20:47:50 -0500

    added p to beginning of pages show route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :business_types
   resources :towns
   resources :markets
+
+  get 'p/:service_id/:town_id', to: 'pages#show'
+  get 't/:market_id/:town_id', to: 'pages#town'
+  get 's/:market_id/:service_id', to: 'pages#service'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
