ffddb5a96aabe71abbb67f36843eed14c235f23c
yoshinator <yoanante@gmail.com>
2020-01-07 20:53:45 -0500

    added pages controller with actions

---
+++ app/controllers/pages.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+
+  # Client will return first client in db if no client found. 
+  # Use first client for default emails. 
+  def show
+    @service = Service.find_by!(slug: params[:service_id])
+    @town = Town.find_by!(slug: params[:town_id])
+    @client = Page.get_client(@town, @service)
+  end 
+
+  def service
+    @service = Service.find_by!(slug: params[:service_id])
+    @market = Market.find_by!(slug: params[:market_id])
+  end
+
+  def town 
+    @town = Town.find_by!(slug: params[:town_id])
+  end
+    
+end
