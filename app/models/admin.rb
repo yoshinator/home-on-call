28075165118cedb2ff5f828b35102b123fcc51d9
yoshinator <yoanante@gmail.com>
2020-01-10 20:10:13 -0500

    added validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,8 @@
 class Admin < ApplicationRecord
   has_secure_password
+
+  validates :password, length: { minimum: 9, allow_blank: true }
+  validates :email, presence: true,
+            format: { with: /\S+@\S+\.\S+/ },
+            uniqueness: {case_sensitive: false}
 end
