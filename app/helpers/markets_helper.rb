a54476c3cf2113b0ae8e1317e35000d336c19ffb
yoshinator <yoanante@gmail.com>
2020-01-10 19:10:42 -0500

    added partials for nav and footer

--- app/helpers/markets_helper.rb
+++ app/helpers/markets_helper.rb
@@ -1,2 +1,10 @@
 module MarketsHelper
+
+  def list_of_markets
+    li = ""
+    Market.all.each do |m|
+      li+="<li>#{m.name}</li>" unless m.name == "Master"
+    end
+    sanitize(li)
+  end
 end
