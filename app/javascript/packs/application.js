8e05c9a1119eced537bc75c786a162ed9d4f3832
yoshinator <yoanante@gmail.com>
2020-01-13 16:15:59 -0500

    added greyscale theme's css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap'
 import './src/application.scss'
+import './src/greyscale.scss'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
