12a3a39709e22b6f048f2a9929577c47ac7b5019
yoshinator <yoanante@gmail.com>
2020-04-04 14:07:36 -0400

    404 hook for custom views

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,13 @@
+class ErrorsController < ApplicationController
+  def not_found
+    render :not_found, status: 404
+  end
+
+  def unacceptable
+    render :unacceptable, status: 422
+  end
+
+  def internal_error
+    render :internal_error, status: 500
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,25 @@
 Rails.application.routes.draw do
 
+  get '/404', to: "errors#not_found"
+  get '/422', to: "erros#unacceptable"
+  get '/500', to: "errors#internal_error"
   root to: 'pages#home'
-  get '/towns/bulk', to: 'towns#bulk_new', as: "new_bulk"
-  post '/towns/bulk', to: 'towns#bulk_create', as: "bulk_town"
+  get '/towns/bulk', to: "towns#bulk_new", as: "new_bulk"
+  post '/towns/bulk', to: "towns#bulk_create", as: "bulk_town"
   
   resources :leads, :admins, :clients, :services, :business_types, :towns, :markets
   resource :session, only: [:new, :create, :destroy]
 
 # Not protected routes. 
   get '/search_results', to: "pages#search_results", as: "search_results"
-  get '/sitemap.xml', to: 'pages#sitemap'
+  get '/sitemap.xml', to: "pages#sitemap"
 
-  get '/s/:service_id', to: 'pages#service', as: "public_service" 
+  get '/s/:service_id', to: "pages#service", as: "public_service" 
   get '/m/:market_id', to: "pages#market", as: "public_market" 
-  get '/m/:market_id/:service_id', to: 'pages#market_service', as: "public_market_service" 
+  get '/m/:market_id/:service_id', to: "pages#market_service", as: "public_market_service" 
 
-  get '/:service_id/:town_id', to: 'pages#show', as: "public_bulk_service"
-  get '/:town_id', to: 'pages#town', as: "public_town" 
+  get '/:service_id/:town_id', to: "pages#show", as: "public_bulk_service"
+  get '/:town_id', to: "pages#town", as: "public_town" 
   
 
 end
