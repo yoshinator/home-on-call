97bb5b8150d409f4b9cc040a35c210dd6c9a4681
yoshinator <yoanante@gmail.com>
2020-04-29 21:18:33 -0400

    created new muxer to add links to the current show page from the current page old muxer became muxer light

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,8 +1,21 @@
 module PagesHelper
   # Location can be town or market. They both have name and state attributes.
-  def muxer (content, location)
-    content.gsub(/\[ror_town\]/, "#{location.name if location.name}, #{location.state if location.state}")
+  def muxer (service, town)
+    content = service.content.gsub(/\[ror_town\]/, "#{town.name if town.name}, #{town.state if town.state}")
+    c = content.match(/(?<link>\+(.*?)\+)/)
+    if c 
+      link =  c[:link]
+      link.gsub!("+", "")
+      link=link_to link, public_bulk_service_path(service, town)
+      return content.gsub(/\+(.*?)\+/, link)
+    else 
+      return content
+    end
   end 
+
+  def muxer_light(content, location)
+    content.gsub(/\[ror_town\]/, "#{location.name if location.name}, #{location.state if location.state}")
+  end
 
   def header_selector(service) 
     if service
