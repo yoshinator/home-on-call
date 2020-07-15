97c69016e6bde03a57337d3e548db67cef6aa64b
yoshinator <yoanante@gmail.com>
2020-07-14 22:25:50 -0400

    test scroll

--- app/javascript/packs/greyscale.js
+++ app/javascript/packs/greyscale.js
@@ -2,9 +2,10 @@
 export default function greyscale(){
   (function ($) {
     "use strict"; // Start of use strict
-
+    
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+      console.log("scrolling")
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
