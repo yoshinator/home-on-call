5230ca1746a79f4e7c90cf1ed59c68e3a28fe0cb
yoshinator <yoanante@gmail.com>
2020-07-29 16:26:09 -0400

    staging

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import './ready'
 import fontawesome from './fontawesome'
 
 fontawesome()
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
