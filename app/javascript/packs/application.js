d58acad9b0508bdc3e19810cc6539b249d5b7d8f
yoshinator <yoanante@gmail.com>
2020-07-29 18:46:03 -0400

    ?

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import './ready'
 import fontawesome from './fontawesome'
 
 fontawesome()
+$(document).on('turbolinks:load', ()=> console.log("FUCKKKK!!"))
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
