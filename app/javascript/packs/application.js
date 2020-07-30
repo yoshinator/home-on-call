f78f61f55785dd46b4f670f58705f38bde787d63
yoshinator <yoanante@gmail.com>
2020-07-30 17:26:07 -0400

    merged

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,14 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 import 'bootstrap'
 import './src/application.scss'
 import './ready'
 import fontawesome from './fontawesome'
 
 fontawesome()
-$(document).on('turbolinks:load', ()=> console.log("FUCKKKK!!"))
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
