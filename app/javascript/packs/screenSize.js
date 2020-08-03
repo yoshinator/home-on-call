1256ebc624fd221c8aec34df2e9d1d3c15f96870
yoshinator <yoanante@gmail.com>
2020-08-03 00:09:54 -0400

    responsive header

--- app/javascript/packs/screenSize.js
+++ app/javascript/packs/screenSize.js
@@ -1,16 +1,26 @@
 $(document).on('ready turbolinks:load', ()=>{
-el = $('.placeholder1')
+  el = $('.placeholder1')
 
-el.get().forEach(img => {
   width = $(window).width();
   mobile_breakpoint = 480;
-  
-  if (width <= mobile_breakpoint) {
-    url = img.dataset.mobileUrl;
-  } else {
-    url = img.dataset.desktopUrl;
-  }
-  img.src =  url
-})
 
+  //homepage images
+  el.get().forEach(img => {
+    if (width <= mobile_breakpoint) {
+      url = img.dataset.mobileUrl;
+    } else {
+      url = img.dataset.desktopUrl;
+    }
+    img.src =  url
+  })
+
+  // mast head on homepage only
+  el2 = $('.root-mast')
+  data = el2.data()
+  if (width <= mobile_breakpoint) {
+    url = data.mobileUrl;
+  } else {
+    url = data.desktopUrl;
+  }
+  el2.css("background", `url(${url})`)
 })
