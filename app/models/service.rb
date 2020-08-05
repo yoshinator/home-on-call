11355bec011ec3c13883a611b90a33ef218fd54a
yoshinator <yoanante@gmail.com>
2020-08-04 22:09:58 -0400

    testing public image url

--- app/models/service.rb
+++ app/models/service.rb
@@ -12,6 +12,18 @@ class Service < ApplicationRecord
     slug
   end
 
+
+  def public_image_url
+    if self.image&.attachment
+        if Rails.env.development?
+            self.image_url = Rails.application.routes.url_helpers.rails_blob_url(self.image, only_path: true)
+        else
+            self.image_url = self.image&.service_url&.split("?")&.first
+        end
+    end
+    #set a default lazily
+    self.image_url ||= ActionController::Base.helpers.asset_path("home_service_bathroom_mobile.jpg")
+end
   private 
   def set_slug 
     self.slug = self.title.parameterize
