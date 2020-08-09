b5e655215d3970abd4e89f9aa81cfaf78042802a
yoshinator <yoanante@gmail.com>
2020-08-09 15:51:17 -0400

    tinyMce issues

--- app/javascript/packs/fontawesome.js
+++ app/javascript/packs/fontawesome.js
@@ -6,8 +6,9 @@ import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons/faMapMarkedAlt
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
 import { faMobileAlt } from '@fortawesome/free-solid-svg-icons/faMobileAlt';
 import { faPaperPlane } from '@fortawesome/free-regular-svg-icons/faPaperPlane';
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars.js';
 
 export default function fontawesome(){
-  library.add(faTwitter, faFacebookF, faYoutube, faMobileAlt, faMapMarkedAlt, faEnvelope, faPaperPlane)
+  library.add(faTwitter, faFacebookF, faYoutube, faMobileAlt, faMapMarkedAlt, faEnvelope, faPaperPlane, faBars)
   dom.watch()
 }
--- app/javascript/packs/tinyMce.js
+++ app/javascript/packs/tinyMce.js
@@ -12,16 +12,19 @@ import 'tinymce/plugins/code';
 import 'tinymce/plugins/wordcount';
 import oxide from './src/skins/ui/oxide-dark/skin.min.css';
 
-(function (){
-  const newLocal = '/styles/content.min.css';
-  tinymce.init({
-    selector: '.tiny',
-    skin: false,
-    // some other settings, like height, language,         
-    // order of buttons on your toolbar etc.
-    height: 400,
-    content_css: newLocal, // <-- this is in the public folder
-    plugins: ['table', 'lists', 'paste', 'link', 'autosave', 'code', 'wordcount'],
-    skin_url: oxide
-  });
-})()
+document.addEventListener("turbolinks:load", ()=> {
+
+    const newLocal = '/styles/content.min.css';
+    tinymce.init({
+      selector: '.tiny',
+      skin: false,
+      // some other settings, like height, language,         
+      // order of buttons on your toolbar etc.
+      height: 400,
+      content_css: newLocal, // <-- this is in the public folder
+      plugins: ['table', 'lists', 'paste', 'link', 'autosave', 'code', 'wordcount'],
+      skin_url: oxide
+    });
+
+})
+
