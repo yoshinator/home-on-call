bc846b2c0e2ee6eb4818fe1df75628ccd983cbf6
yoshinator <yoanante@gmail.com>
2020-08-09 14:56:26 -0400

    errors was an erroneous ;

--- app/javascript/packs/fontawesome.js
+++ app/javascript/packs/fontawesome.js
@@ -1,11 +1,11 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import faTwitter from '../../../node_modules/@fortawesome/free-brands-svg-icons/faTwitter'
-import faFacebookF from '../../../node_modules/@fortawesome/free-brands-svg-icons/faFacebookF';
-import faYoutube from '../../../node_modules/@fortawesome/free-brands-svg-icons/faYoutube';
-import faMapMarkedAlt from '../../../node_modules/@fortawesome/free-solid-svg-icons/faMapMarkedAlt';
-import faEnvelope from '../../../node_modules/@fortawesome/free-solid-svg-icons/faEnvelope';
-import faMobileAlt from '../../../node_modules/@fortawesome/free-solid-svg-icons/faMobileAlt';
-import faPaperPlane from '../../../node_modules/@fortawesome/free-regular-svg-icons/faPaperPlane;'
+import faTwitter from '@fortawesome/free-brands-svg-icons/faTwitter'
+import faFacebookF from '@fortawesome/free-brands-svg-icons/faFacebookF';
+import faYoutube from '@fortawesome/free-brands-svg-icons/faYoutube';
+import faMapMarkedAlt from '@fortawesome/free-solid-svg-icons/faMapMarkedAlt';
+import faEnvelope from '@fortawesome/free-solid-svg-icons/faEnvelope';
+import faMobileAlt from '@fortawesome/free-solid-svg-icons/faMobileAlt';
+import faPaperPlane from '@fortawesome/free-regular-svg-icons/faPaperPlane';
 
 export default function fontawesome(){
   library.add(faTwitter, faFacebookF, faYoutube, faMobileAlt, faMapMarkedAlt, faEnvelope, faPaperPlane)
