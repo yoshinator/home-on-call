ef98e2e547eb863ce62baa33c61c64b0cf995dd6
yoshinator <yoanante@gmail.com>
2020-08-14 19:56:03 -0400

    added more font weights and also way to embed cta in content

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,10 @@
 module PagesHelper
   def muxer (service, town)
+    # replace [ror_town] with town name and town state
     content = service.content.gsub(/\[ror_town\]/, "#{town.name if town.name}, #{town.state if town.state}")
+
+    content = content.gsub(/\[cta\]/, "<a href='#lead-picker' class='btn btn-primary d-block js-scroll-trigger'>Get Started For Free</a>")
+    # replace items between  plus sign ++ with a link to the same page.
     c = content.match(/(?<link>\+(.*?)\+)/)
     if c 
       link =  c[:link]
@@ -12,7 +16,8 @@ module PagesHelper
     end
   end 
   
-  # Location can be town or market. They both have name and state attributes.
+  # Location can be town or market. They both have name and state attributes. Used 
+  # for market show page
   def muxer_light(content, location)
     content.gsub(/\[ror_town\]/, "#{location.name if location.name}, #{location.state if location.state}")
   end
