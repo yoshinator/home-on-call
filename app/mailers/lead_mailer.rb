e6babca4c491cb2af55cad4ef6ddce83de412910
Yoan Ante <yoanante@gmail.com>
2020-08-19 23:50:51 -0400

    mailer

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -2,6 +2,6 @@ class LeadMailer < ApplicationMailer
     def new_lead_email
     @lead = params[:lead]
     @email = @lead.client.email
-    mail(to: @email, subject: "You got a new order!")
+    mail(to: @email, bcc: ["yoanante@gmail.com"], subject: "You got a new lead from Home On Call!")
   end
 end
