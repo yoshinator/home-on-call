8262d38062e15dbc3af2600d599b8e627e41130c
Yoan Ante <yoanante@gmail.com>
2021-08-07 16:35:00 -0400

    remove port from production mailer default url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
 #These settings are for the sending out email for active admin and consequently the   devise mailer
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: ENV['PORT'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
     config.action_mailer.perform_deliveries = true
     config.action_mailer.perform_caching = false
-    config.action_mailer.default_url_options = { host: 'homeoncall.com', port: ENV['PORT'] }
+    config.action_mailer.default_url_options = { host: 'homeoncall.com' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
